4535e6e4d65da508caee188aa4db39808d7e3c53
Joao Raupp <joao.raupp@orgafarma.com>
2026-08-04 14:46:50 -0300

    fixed: correção do metric_name para str

--- app/schemas/alert_schema.py
+++ app/schemas/alert_schema.py
@@ -1,17 +1,16 @@
 import uuid
 from datetime import datetime
-
-from app.models.alert import AlertSeverity, AlertStatus, MetricType
+from app.models.alert import AlertSeverity, AlertStatus
 from app.schemas.base_schema import BaseSchema, BaseResponseSchema
 
-
+#class AlertCreate é uma classe para criar alertas, herdando os campos da classe BaseSchema e adicionando campos específicos como server_id, severity, status, metric_name, metric_value, threshold, title, description e resolved_at.
 class AlertResponse(BaseResponseSchema):
     server_id: uuid.UUID
 
     severity: AlertSeverity
     status: AlertStatus
 
-    metric_name: MetricType
+    metric_name: str
     metric_value: float
     threshold: float
 
